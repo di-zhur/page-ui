c43b4e9348658447ffdc4ba2580dc24942e3c3fb
di <dmitro.zhuravlev@yandex.ru>
2020-01-26 12:12:26 +0300

    commit

--- src/components/AppRouter/index.js
+++ src/components/AppRouter/index.js
@@ -1,21 +1,10 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useParams,
-  useRouteMatch
-} from "react-router-dom";
-import { Navbar, Nav } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import Home from "../Home";
+import Calculation from "../Calculation";
 
 export default function AppRouter() {
-  const linkStyle = {
-    color: "white",
-    textDecoration: 'none',
-    backgroundColor: 'transparent'
-  };
-
   return (
     <Router>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
@@ -23,74 +12,27 @@ export default function AppRouter() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link>
-              <Link style={linkStyle} to="/">Home</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link style={linkStyle} to="/calculations">Calculations</Link>
-            </Nav.Link>
+            <Nav.Link as={Link} to="/" >Home</Nav.Link>
+            <Nav.Link as={Link} to="/calculations" >Calculations</Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link>Login</Nav.Link>
+            <Nav.Link as={Link} to="/">Login</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/calculations">
-          <Topics />
-        </Route>
-      </Switch>
+      <Container fluid>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/calculations">
+              <Calculation />
+            </Route>
+          </Switch>
+      </Container>
     </Router>
   );
 }
 
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
 
-function Topics() {
-  let { path, url } = useRouteMatch();
 
-  return (
-    <div>
-      <h2>Topics</h2>
-      <ul>
-        <li>
-          <Link to={`${url}/rendering`}>Rendering with React</Link>
-        </li>
-        <li>
-          <Link to={`${url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${url}/props-v-state`}>Props v. State</Link>
-        </li>
-      </ul>
-
-      <Switch>
-        <Route exact path={path}>
-          <h3>Please select a topic.</h3>
-        </Route>
-        <Route path={`${path}/:topicId`}>
-          <Topic />
-        </Route>
-      </Switch>
-    </div>
-  );
-}
-
-function Topic() {
-  let { topicId } = useParams();
-  return (
-    <div>
-      <h3>{topicId}</h3>
-    </div>
-  );
-}
---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+import {Container, Jumbotron} from "react-bootstrap";
+import React from "react";
+
+export default function Home() {
+  return (
+    <Jumbotron fluid>
+      <Container>
+        <h1>Computing application</h1>
+        <p>
+          Computing application...............
+        </p>
+      </Container>
+    </Jumbotron>
+  );
+}
